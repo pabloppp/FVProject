b1a3405b502bae73277183268e4cd5c84f029ec0
Roxanne <roxanne.lopez.dooren@gmail.com>
2015-02-24 09:29:05 +0100

    cambiado color

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Blue);
+    shape.setFillColor(sf::Color::Red);
     while (window.isOpen())
     {
-        sf::Event event;
+        sf::Event eventA;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed) window.close();
