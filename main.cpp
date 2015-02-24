93416db2816667625b2664fecc8c67c6d10956d3
Andrea <andrea_lorente_1994@hotmail.com>
2015-02-24 10:47:38 +0100

    Demo añadida

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 #include "engine/GMEngine.hpp"
 #include "hito1/hito1Game.hpp"
+#include "demo/MyGame.hpp"
 
 int main() {
-    gme::Game *H1G = new hito1Game(sf::Vector2f(640, 480), "hito1 game");
-    
+    //gme::Game *H1G = new hito1Game(sf::Vector2f(640, 480), "hito1 game");
+    gme::Game *H1G = new MyGame(sf::Vector2f(640, 480), "My game");
+
     H1G->run();
     
     delete H1G;
